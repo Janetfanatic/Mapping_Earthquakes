9b21c03b5fe383b0e35d231c781d92e3aadc1b8d
Charlene Glasgow <And2reaC@aol.com>
2020-08-13 23:56:07 -0400

    updated code

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,4 +1,4 @@
-/ Add console.log to check to see if our code is working.
+// Add console.log to check to see if our code is working.
 console.log("working");
 
 let map = L.map('mapid').setView([40.7, -94.5], 4);
